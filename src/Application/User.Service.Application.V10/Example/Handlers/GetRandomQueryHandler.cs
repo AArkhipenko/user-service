262390e6ddd45202953f98c4564ec4f861fa8ec9
AArkhipenko <arkhipenko.alex95@gmail.com>
2024-02-04 12:42:45 +0500

    Создан контроллер с примерами использования

---
+++ src/Application/User.Service.Application.V10/Example/Handlers/GetRandomQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using User.Service.Application.V10.Example.Queries;
+using User.Service.Domain.Core.Logging;
+
+namespace User.Service.Application.V10.Example.Hadlers
+{
+	/// <summary>
+	/// Выполнение <see cref="GetRandomQuery"/>
+	/// </summary>
+	internal class GetRandomQueryHandler: LoggerWrapper, IRequestHandler<GetRandomQuery, IEnumerable<int>>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GetRandomQueryHandler"/> class.
+		/// </summary>
+		/// <param name="logger"><see cref="ILogger"/></param>
+		public GetRandomQueryHandler(
+			ILogger<GetRandomQueryHandler> logger)
+			: base(logger) { }
+
+		/// <inheritdoc/>
+		public Task<IEnumerable<int>> Handle(GetRandomQuery request, CancellationToken cancellationToken)
+		{
+			using (_ = BeginLoggingScope())
+			{
+				var result = Enumerable.Range(1, 5)
+					.Select(index => Random.Shared.Next(-20, 55))
+					.ToList();
+				return Task.FromResult(result.AsEnumerable());
+			}
+		}
+	}
+}
