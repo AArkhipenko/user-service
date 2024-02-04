736c0c9eb70b547e833af83e407a533baa3dec11
AArkhipenko <arkhipenko.alex95@gmail.com>
2024-02-04 12:42:05 +0500

    Добавлена обертка над логированием
    Создан класс для автоматического указания раздела логирования

---
+++ src/Domain/User.Service.Domain.Core/Logging/LoggerWrapperScope.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.Service.Domain.Core.Logging
+{
+	/// <summary>
+	/// Класс объекта раздела логирования
+	/// </summary>
+	public class LoggerWrapperScope: IDisposable
+	{
+		private ILogger _logger;
+		private IDisposable _scope;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LoggerWrapperScope"/> class.
+		/// </summary>
+		/// <param name="logger"><see cref="ILogger"/></param>
+		/// <param name="scopeName">название раздела логирования</param>
+		/// <exception cref="ArgumentNullException">не зедан входной параметр</exception>
+		/// <exception cref="Exception">не удалось создать объект раздела логирования</exception>
+		public LoggerWrapperScope(ILogger logger, string scopeName)
+		{
+			if (scopeName is null)
+			{
+				throw new ArgumentNullException(nameof(logger));
+			}
+
+			this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			this._scope = this._logger.BeginScope(scopeName) ?? throw new Exception("Не создан объект логирования раздела");
+
+			this._logger.LogInformation("Начало логирования раздела");
+		}
+
+		/// <inheritdoc/>
+		public void Dispose()
+		{
+			this._logger.LogInformation("Завершение логирования раздела");
+			this._scope.Dispose();
+		}
+	}
+}
---
+++ src/User.Service.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using User.Service.Domain.Core.Logging;
+
+namespace User.Service.Api.Controllers
+{
+	/// <summary>
+	/// Базовый контроллер для всех контроллеров
+	/// Реализует <see cref="ILoggerWrapper"/>
+	/// </summary>
+	public class ApiBaseController : ControllerBase, ILoggerWrapper
+    {
+        private readonly LoggerWrapper _loggerWrapper;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ApiBaseController"/> class.
+		/// </summary>
+		/// <param name="logger"><see cref="ILogger"/></param>
+		public ApiBaseController(ILogger logger)
+        {
+            this._loggerWrapper = new LoggerWrapper(logger);
+        }
+
+        /// <inheritdoc />
+        public ILogger Logger { get => this._loggerWrapper.Logger; }
+
+		/// <inheritdoc />
+		[ApiExplorerSettings(IgnoreApi = true)]
+		public LoggerWrapperScope BeginLoggingScope([CallerMemberName] string? myCallerName = null, string? callerClassName = null)
+            => this._loggerWrapper.BeginLoggingScope(myCallerName, callerClassName ?? this.GetType().Name);
+    }
+}
