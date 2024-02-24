3a67ae1fe3aea7e9deca21eeee50ed1b7dbf5bc6
AArkhipenko <arkhipenko.alex95@gmail.com>
2024-02-24 11:50:12 +0500

    Добавлены тесты
    Созданы проекты с тестами на все проекты
    Создан тесты на АПИ GET /examples/v10

---
+++ test/User.Service.API.Tests/Controllers/V10/ExampleController/GetAsyncTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Controller = User.Service.API.Controllers.V10.ExampleController;
+
+namespace User.Service.API.Tests.Controllers.V10.ExampleController
+{
+	/// <summary>
+	/// Тесты на метод <see cref="Controller.GetAsync(CancellationToken)"/>
+	/// </summary>
+    public class GetAsyncTests
+    {
+		/// <summary>
+		/// Корректное выполнение АПИ
+		/// </summary>
+		/// <returns>OK</returns>
+		[Fact]
+		public async Task It_returns_OK()
+		{
+			// Настройки приложения
+			var testClient = TestBase.CreateClient(services => { });
+
+			// Выполнение запроса
+			var response = await testClient.GetAsync("/examples/v10");
+
+			// Тесты результата
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+			var content = await response.ReadAsJsonAsync<IEnumerable<int>>();
+
+			Assert.NotNull(content);
+			Assert.NotEmpty(content);
+		}
+    }
+}
---
+++ test/User.Service.API.Tests/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace User.Service.API.Tests
+{
+	/// <summary>
+	/// Базовый тестовый класс с различными методами для подготовки к тестированию
+	/// </summary>
+	internal class TestBase
+	{
+		/// <summary>
+		/// Создание тестового http-клиента для проверки АПИ
+		/// </summary>
+		/// <returns><see cref="HttpClient"/></returns>
+		public static HttpClient CreateClient(Action<IServiceCollection> configureServices) {
+			var server = new WebApplicationFactory<Program>()
+				.WithWebHostBuilder(builder =>
+				{
+					builder.ConfigureServices(configureServices);
+				});
+			var client = server.CreateClient();
+			return client;
+		}
+	}
+}
