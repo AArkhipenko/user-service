0e3560b8f895a6c4867f8a5322f21da37f52dc8d
AArkhipenko <arkhipenko.alex95@gmail.com>
2024-11-13 17:10:05 +0500

    Переименовывание решения
    Переименовывание решения, проектов, папок

---
+++ src/User.Service.API/Authority/ResourceInnerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace User.Service.API.Authority
+{
+	/// <summary>
+	/// Модель внутренностей раздела resource_access
+	/// </summary>
+	public class ResourceInnerModel
+	{
+		/// <summary>
+		/// Роли пользователя в рамках 
+		/// </summary>
+		public IEnumerable<string> Roles { get; set; } = Enumerable.Empty<string>();
+	}
+}
