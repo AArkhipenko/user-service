5ba6f2d7354b9be735fd934bf3b0af62033f2378
AArkhipenko <arkhipenko.alex95@gmail.com>
2025-02-15 10:57:36 +0500

    AKP-1: Добавлен репозитория для работы с пользователями

---
+++ src/Infrastructure/User.Service.Infrastructure.EF/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using User.Service.Domain.Core.Exceptions;
+using User.Service.Domain.Interface.Models;
+using User.Service.Domain.Interface.Repositories;
+
+namespace User.Service.Infrastructure.EF.Repositories
+{
+	/// <summary>
+	/// Реализация <see cref="IUserRepository"/>
+	/// </summary>
+	internal class UserRepository : IUserRepository
+	{
+		private readonly PublicContext _context;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UserRepository"/> class.
+		/// </summary>
+		/// <param name="context"><see cref="PublicContext"/></param>
+		public UserRepository(PublicContext context)
+		{
+			this._context = context ?? throw new ArgumentNullException(nameof(context));
+		}
+
+		/// <inheritdoc/>
+		public async Task<FullUserModel> GetFullUserAsync(int userId)
+		{
+			var userDb = await this._context.Users.FindAsync(userId);
+			if (userDb is null)
+			{
+				throw new NotFoundException($"Не найден пользователь с id={userId}");
+			}
+
+			var personDb = await this._context.Persons.FindAsync(userId);
+			if (personDb is null)
+			{
+				throw new NotFoundException($"Не найдена персональная информация для пользователя с id={userId}");
+			}
+
+			return new FullUserModel(
+				userDb.Id,
+				userDb.UserTypeId,
+				personDb.FirstName,
+				personDb.SecondName,
+				personDb.LastName,
+				personDb.BirthDay,
+				personDb.Email,
+				personDb.Phone);
+		}
+	}
+}
