6ae7d7930dc0c17c775c162af0f6ee683d84f50a
AArkhipenko <arkhipenko.alex95@gmail.com>
2025-02-15 10:56:07 +0500

    AKP-1: Настройка работы с бД
    1. Добавлены таблицы
    2. Добавлены конфигурации таблиц
    3. Добавлен контекст БД

---
+++ src/Infrastructure/User.Service.Infrastructure.EF/Configurations/UserConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using TableExt = User.Service.Infrastructure.EF.Tables;
+
+namespace User.Service.Infrastructure.EF.Configurations
+{
+	/// <summary>
+	/// Настройка отображения таблицы на модель
+	/// </summary>
+	internal class UserConfiguration : IEntityTypeConfiguration<TableExt.User>
+	{
+		/// <inheritdoc/>
+		public void Configure(EntityTypeBuilder<TableExt.User> builder)
+		{
+			builder.ToTable("users", "public")
+				.HasKey(k => k.Id);
+
+			builder
+				.Property<int>(p => p.Id)
+				.HasColumnName("id")
+				.ValueGeneratedOnAdd();
+
+			builder
+				.Property<string>(p => p.ExternalId)
+				.HasColumnName("external_id")
+				.IsRequired();
+
+			builder
+				.Property<int>(p => p.UserTypeId)
+				.HasColumnName("user_type_id")
+				.IsRequired();
+
+			builder
+				.HasOne(x => x.UserType)
+				.WithOne()
+				.HasForeignKey<TableExt.User>(x => x.UserTypeId);
+		}
+	}
+}
